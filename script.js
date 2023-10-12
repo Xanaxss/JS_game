5e7d690c2c3ac0c10793de89ab41c40dfc613aae
unknown <kartamax2705@gmail.com>
2023-10-12 10:31:54 +0900

    v0.02

--- script.js
+++ script.js
@@ -115,3 +115,83 @@ function draw_boom(){
   x = (x === 512 ? 0 : x + 50);
   ctx.drawImage(sprite_boom, 1000, 1000);
 }
+
+
+
+////////////////////Sprite///////////////////////////////////////////////////////
+
+
+class Sprite {
+    constructor(options) {
+        this.ctx = options.ctx;
+
+        this.image = options.image;
+
+        this.frameIndex = 0;
+        this.tickCount = 0;
+        this.ticksPerFrame = options.ticksPerFrame || 0;
+        this.numberOfFrames = options.numberOfFrames || 1;
+
+        this.width = options.width;
+        this.height = options.height;
+
+        this.start();
+    }
+
+    update() {
+        this.tickCount++;
+
+        if (this.tickCount > this.ticksPerFrame) {
+            this.tickCount = 0;
+            if (this.frameIndex < this.numberOfFrames - 1) {
+                this.frameIndex++;
+            } else {
+                this.frameIndex = 0;
+            }
+        }
+    }
+
+    render() {
+        this.ctx.drawImage(
+            this.image,
+            this.frameIndex * this.width / this.numberOfFrames,
+            0,
+            this.width / this.numberOfFrames,
+            this.height,
+            0,
+            0,
+            this.width / this.numberOfFrames,
+            this.height
+        )
+    }
+
+    start() {
+        let loop = () => {
+            this.update();
+            this.render();
+
+            window.requestAnimationFrame(loop);
+        }
+
+        window.requestAnimationFrame(loop);
+    }
+}
+
+
+////////////////////////////////////////////
+
+
+
+
+let shipImg = new Image();
+shipImg.src = './img/sprites.png';
+let ship = new Sprite({
+  ctx: canvas.getContext('2d'),
+  image: shipImg,
+  width: 200,
+  height: 80,
+  x: 100,
+  y: 100,
+  numberOfFrames: 4,
+  ticksPerFrame: 8,
+})
